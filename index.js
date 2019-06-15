20224404c86ff575776c2b1d45a2b9ab9b7983e3
Rishabh <rishabhkanojiya75@gmail.com>
2019-06-15 15:22:53 +0530

    Updating

--- index.js
+++ index.js
@@ -29,12 +29,12 @@ require("./routes/authRoutes")(app);
 require("./routes/paymentRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "client/build")));
+  app.use(express.static("client/build"));
 
   const path = require("path");
 
-  app.get("*", function(req, res) {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
